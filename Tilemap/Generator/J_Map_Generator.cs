ffa4e5754e0c89ad666324c0f1de06998a0e9762
JackMariani <giacomo.p.mariani@gmail.com>
2019-11-16 13:18:41 +0200

    adjusted boundaries

--- Tilemap/Generator/J_Map_Generator.cs
+++ Tilemap/Generator/J_Map_Generator.cs
@@ -8,6 +8,7 @@ namespace JReact.Tilemaps.Generator
     public abstract class J_Map_Generator<T> : MonoBehaviour
         where T : J_Tile
     {
+        private const int zPos = 0;
         // --------------- CREATORS --------------- //
         [BoxGroup("Setup", true, true, 0), SerializeField, AssetsOnly, Required] protected J_TileRetriever _tileInfoGetter;
         // --------------- UNITY TILEMAPS --------------- //
@@ -15,8 +16,12 @@ namespace JReact.Tilemaps.Generator
         [BoxGroup("Setup", true, true, 0), SerializeField, Required] protected Tilemap _groundTileMap;
 
         // --------------- THE MAP GRID --------------- //
+        [InfoBox("NULL => No Boundaries"), BoxGroup("Setup", true, true, 0), SerializeField, AssetsOnly]
+        private J_TileInfo _boundary;
         [BoxGroup("Setup", true, true, 0), SerializeField] protected Vector3Int _startPoint;
         [BoxGroup("Setup", true, true, 0), SerializeField] protected int _width;
+        [BoxGroup("Setup", true, true, 0), SerializeField] protected int _Height => _GroundCodes.Length / _width;
+
         // --------------- ABSTRACT --------------- //
         protected abstract JMapGrid<T> _MapGrid { get; }
         protected abstract iIntArrayGetter _GroundCodes { get; }
@@ -26,27 +31,18 @@ namespace JReact.Tilemaps.Generator
         public void Generate()
         {
             // --------------- INITIATION --------------- //
-           if (_GroundCodes == null || _GroundCodes.Length % _width != 0)
-                throw new ArgumentException($"{name} {nameof(_GroundCodes)} is not divisible for width {_width}. Not enough columns");
+            if (_GroundCodes                 == null ||
+                _GroundCodes.Length % _width != 0)
+                throw new ArgumentException($"{name} {nameof(_GroundCodes)} is null {_GroundCodes == null}, " +
+                                            $"or not divisible for width {_width}. Not enough columns");
 
-            var allTiles = new T[_GroundCodes.Length];
             Initiate();
 
+            // --------------- BOUNDARIES  --------------- //
+            if (_boundary != null) DrawBoundaries(_boundary, _width, _Height);
+
             // --------------- CREATION --------------- //
-            for (int i = 0; i < _GroundCodes.Length; i++)
-            {
-                // --------------- POSITION --------------- //
-                Vector3Int position = new Vector3Int { x = i % _width, y = i / _width };
-                position += _startPoint;
-
-                // --------------- TILE --------------- //
-                var groundTileInfo = _tileInfoGetter.GetItemFromId(_GroundCodes.ArrayCode[i]);
-                var tile           = CreateTile(i, position, groundTileInfo);
-
-                // --------------- STORE TILE --------------- //
-                groundTileInfo.Add(tile);
-                allTiles[i] = tile;
-            }
+            var allTiles = FillMapWithTiles();
 
             // --------------- CONFIRM --------------- //
             _MapGrid.InitiateMap(_unityMapGrid, allTiles, _width);
@@ -57,6 +53,68 @@ namespace JReact.Tilemaps.Generator
         {
             _tileInfoGetter.ClearTileList();
             _groundTileMap.ClearAllTiles();
+        }
+
+        // --------------- CREATION --------------- //
+        private void DrawBoundaries(J_TileInfo boundary, int width, int height)
+        {
+            // --------------- CALCULATE EDGES --------------- //
+            int westEdge  = _startPoint.x - 1;
+            int eastEdge  = _startPoint.x + width;
+            int southEdge = _startPoint.y - 1;
+            int northEdge = _startPoint.y + height;
+
+            // --------------- VERTICAL --------------- //
+            for (int i = southEdge + 1; i < northEdge; i++)
+            {
+                Vector3Int boundaryWestPos = new Vector3Int(westEdge, i, zPos);
+                Vector3Int boundaryEastPos = new Vector3Int(eastEdge, i, zPos);
+
+                var westTile = CreateBoundary(boundaryWestPos, boundary);
+                boundary.Add(westTile);
+                var eastTile = CreateBoundary(boundaryEastPos, boundary);
+                boundary.Add(eastTile);
+            }
+
+            // --------------- HORIZONTAL --------------- //
+            for (int i = westEdge; i < eastEdge +1; i++)
+            {
+                Vector3Int boundarySouthPos = new Vector3Int(i, southEdge, zPos);
+                Vector3Int boundaryNorthPos = new Vector3Int(i, northEdge, zPos);
+            
+                var southTile = CreateBoundary(boundarySouthPos, boundary);
+                boundary.Add(southTile);
+                var northTile = CreateBoundary(boundaryNorthPos, boundary);
+                boundary.Add(northTile);
+            }
+        }
+
+        protected virtual J_Tile CreateBoundary(Vector3Int position, J_TileInfo groundTileInfo)
+        {
+            var tile = new J_Tile(position, _groundTileMap, groundTileInfo);
+            return tile;
+        }
+
+        private T[] FillMapWithTiles()
+        {
+            var tiles = new T[_GroundCodes.Length];
+
+            for (int i = 0; i < _GroundCodes.Length; i++)
+            {
+                // --------------- POSITION --------------- //
+                Vector3Int position = new Vector3Int(i % _width, i / _width, zPos);
+                position += _startPoint;
+
+                // --------------- TILE --------------- //
+                var groundTileInfo = _tileInfoGetter.GetItemFromId(_GroundCodes.ArrayCode[i]);
+                var tile           = CreateTile(i, position, groundTileInfo);
+
+                // --------------- STORE TILE --------------- //
+                groundTileInfo.Add(tile);
+                tiles[i] = tile;
+            }
+
+            return tiles;
         }
 
         // --------------- ABSTRACT --------------- //
