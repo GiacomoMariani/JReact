587babbc6294b5521e56d07b8994ab08e34e7701
JackMariani <giacomo.p.mariani@gmail.com>
2020-08-23 15:26:26 +0200

    minor adjustments

--- JExtensions.cs
+++ JExtensions.cs
@@ -62,7 +62,7 @@ namespace JReact
         }
 
         /// <summary>
-        /// deconverts a byt to axis
+        /// converts a byte to axis
         /// </summary>
         /// <param name="axisByte">the byte to deconvert</param>
         /// <returns>returns the axis</returns>
@@ -97,85 +97,6 @@ namespace JReact
         public static T ToEnum<T>(this string enumString, bool caseSensitive = false)
             => (T) Enum.Parse(typeof(T), enumString, caseSensitive);
         #endregion
-
-        #region ARRAYS
-        /// <summary>
-        /// checks if an array contains a given item
-        /// </summary>
-        /// <param name="array">the array to check</param>
-        /// <param name="itemToCheck">the item we want to find</param>
-        /// <returns>returns true if the array contains the item</returns>
-        public static bool ArrayContains<T>(this T[] array, T itemToCheck) => Array.IndexOf(array, itemToCheck) > -1;
-
-        public static bool ArrayIsValid<T>(this T[] array) => array != null && array.Length > 0;
-
-        public static bool ValidIndex<T>(this T[] array, int index) => array != null && index < array.Length && index > 0;
-
-        public static T[] SubArray<T>(this T[] data, int index, int length)
-        {
-            T[] result = new T[length];
-            Array.Copy(data, index, result, 0, length);
-            return result;
-        }
-
-        public static T[] AddItemToArray<T>(this IEnumerable<T> data, T item)
-        {
-            List<T> tempList = data.ToList();
-            tempList.Add(item);
-            return tempList.ToArray();
-        }
-        #endregion ARRAYS
-
-        #region COLLECTIONS
-        public static void SubscribeToAll<T>(this IEnumerable<T> collection, Action actionToPerform)
-            where T : jObservable
-        {
-            foreach (T element in collection) element.Subscribe(actionToPerform);
-        }
-
-        public static void UnSubscribeToAll<T>(this IEnumerable<T> collection, Action actionToPerform)
-            where T : jObservable
-        {
-            foreach (T element in collection) element.UnSubscribe(actionToPerform);
-        }
-
-        public static void SubscribeToAllEnd<T>(this IEnumerable<T> collection, Action action)
-            where T : J_State
-        {
-            foreach (T element in collection) element.SubscribeToEnd(action);
-        }
-
-        public static void UnSubscribeToAllEnd<T>(this IEnumerable<T> collection, Action action)
-            where T : J_State
-        {
-            foreach (T element in collection) element.UnSubscribeToEnd(action);
-        }
-
-        public static void SubscribeToAll<T>(this IEnumerable<jObservable<T>> collection, Action<T> actionToPerform)
-        {
-            foreach (jObservable<T> element in collection) element.Subscribe(actionToPerform);
-        }
-
-        public static void UnSubscribeToAll<T>(this IEnumerable<jObservable<T>> collection, Action<T> actionToPerform)
-        {
-            foreach (jObservable<T> element in collection) element.UnSubscribe(actionToPerform);
-        }
-
-        public static void ActivateAll(this IEnumerable<J_Service> services)
-        {
-            foreach (J_Service element in services) element.Activate();
-        }
-
-        public static void UnSubscribeToAll<T>(this IEnumerable<J_Service> services)
-        {
-            foreach (J_Service element in services) element.End();
-        }
-
-        public static void ResetAll(IEnumerable<iResettable> collection)
-        {
-            foreach (iResettable element in collection) element.ResetThis();
-        }
-        #endregion COLLECTIONS
 
         #region SCRIPTABLE OBJECTS
         //a way to set the names of scriptable object
